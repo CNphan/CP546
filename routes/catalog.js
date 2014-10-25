9d45d59226415b1884eec0eb60226a1832029d49
Brandon L. Clark <support@themindspot.com>
2014-10-25 16:26:29 -0700

    Made code and spelling correction to merged work so we can discuss interface next meeting.

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,26 +1,26 @@
 module.exports = function (data) {
 	var express = require('express');
 	var router = express.Router();
-	
+
 	/* GET catalog page. */
 	router.get('/', function(req, res) {
 		res.render('catalog', { title: 'University Manager | Class Catalog', user: data.user });
 	});
-	
+
 	/* GET schedule page. */
 	router.get('/schedule', function(req, res) {
 		res.render('schedule', { title: 'University Manager | My Schedule', user: data.user });
 	});
-	
+
 	/* GET add page. */
 	router.get('/addcourse', function(req, res) {
 		res.render('addcourse', { title: 'University Manager | Add Class Schedule', user: data.user });
 	});
-	
+
 	/* GET add page. */
 	router.get('/addschedule', function(req, res) {
-		res.render('addschedule', { title: 'University Manager | Class Scheduled added', user: data.user });
+		res.render('addschedule', { title: 'University Manager | Class Scheduled added' });
 	});
-	
+
 	return router;
 };
