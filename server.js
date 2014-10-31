279490a1a1b10ddea4bf86666c617b89f7251880
Brandon L. Clark <support@themindspot.com>
2014-10-31 13:28:52 -0700

    Fix for create account and ports

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var options = {
     requestCert: true
 };
 
-app.set('port-ssl', process.env.PORT || 3443);
-app.set('port', process.env.PORT || 3000);
+app.set('port-ssl', process.env.PORT || 443);
+app.set('port', process.env.PORT || 80);
 
 var server = https.createServer(options, app).listen(app.get('port-ssl'), function() {
   console.log('Secure Express server listening on port ' + server.address().port);
