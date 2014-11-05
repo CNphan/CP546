a8ce1e20e6bb59d59adab420ba5780d3f0d212c2
Brandon L. Clark <support@themindspot.com>
2014-11-05 10:38:42 -0800

    Minor catalog bug

--- data/user.js
+++ data/user.js
@@ -98,84 +98,85 @@ module.exports.getUserById = function(id, cb){
 		.exec(function(err, user){
 			if (err){cb(err, null);return;}
 			
-			profile = user.getData();
-			// search for detail object
-			
-			if(user.detail){
-				UserDetail
-					.findOne({detail:user.detail._id})
-					.exec(function(err, details){
-						if (err) {cb(err, null);return;}
-						
-						profile.detail = [];
-						profile.detail = details.getData();
-						
-						//search for contact objects
-						var contactId = [];
-						profile.detail.contact = [];
-						for (n in details.contact){
-							if(n == 0) {contactId.push(details.contact[n]);}
-						}
-						console.log(profile);
-						for(var i =0;i<details.contact.length; i++){
-							UserContact
-								.find({_id:contactId[i]})
-								.exec(function(err, contact){	
-									if (err) {cb(err, null);return;}
-									
-									for(n in contact){
-										profile.detail.contact.push(contact[n].getData());
-									}
-								});
-						}
-						console.log(profile);
-						// search for transcript history object
-						if(details.transcripts){
-							UserTranscriptHistory
-								.findOne({_id:details.transcripts})
-								.exec(function(err, history){
-									if (err) {cb(err, null);return;}
-									
-									profile.detail.transcripts = history.getData();
-									
-									//search for college transcript objects
-									var transcriptId = [];
-									profile.detail.transcripts.college = [];
-									for (var i = 0; i < history.college.length; i++){
-										transcriptId.push(history.college[i]);
-									}
-									for(var j = 0; j < transcriptId.length; j++){
+			if(user){
+				profile = user.getData();
+				// search for detail object
+				
+				if(user.detail){
+					UserDetail
+						.findOne({detail:user.detail._id})
+						.exec(function(err, details){
+							if (err) {cb(err, null);return;}
+							
+							profile.detail = [];
+							profile.detail = details.getData();
+							
+							//search for contact objects
+							var contactId = [];
+							profile.detail.contact = [];
+							for (n in details.contact){
+								if(n == 0) {contactId.push(details.contact[n]);}
+							}
+							console.log(profile);
+							for(var i =0;i<details.contact.length; i++){
+								UserContact
+									.find({_id:contactId[i]})
+									.exec(function(err, contact){	
+										if (err) {cb(err, null);return;}
+										
+										for(n in contact){
+											profile.detail.contact.push(contact[n].getData());
+										}
+									});
+							}
+							// search for transcript history object
+							if(details.transcripts){
+								UserTranscriptHistory
+									.findOne({_id:details.transcripts})
+									.exec(function(err, history){
+										if (err) {cb(err, null);return;}
+										
+										profile.detail.transcripts = history.getData();
+										
+										//search for college transcript objects
+										var transcriptId = [];
+										profile.detail.transcripts.college = [];
+										for (var i = 0; i < history.college.length; i++){
+											transcriptId.push(history.college[i]);
+										}
+										for(var j = 0; j < transcriptId.length; j++){
+											UserTranscript
+												.findOne({_id:transcriptId[j]})
+												.exec(function(err, transcripts){
+													if (err) {cb(err, null);return;}
+													
+													profile.detail.transcripts.college.push(transcripts.getData());
+												});
+										}
+										
+										// search for ge transcript object
 										UserTranscript
-											.findOne({_id:transcriptId[j]})
-											.exec(function(err, transcripts){
+											.findOne({_id:history.general})
+											.exec(function(err, ge){
 												if (err) {cb(err, null);return;}
 												
-												profile.detail.transcripts.college.push(transcripts.getData());
+												profile.detail.transcripts.general = ge.getData();
+												db.close();
+												cb(null, profile);
+												return;
 											});
-									}
-									
-									// search for ge transcript object
-									UserTranscript
-										.findOne({_id:history.general})
-										.exec(function(err, ge){
-											if (err) {cb(err, null);return;}
-											
-											profile.detail.transcripts.general = ge.getData();
-											db.close();
-											cb(null, profile);
-											return;
-										});
-								});
-						} else {
-							db.close();
-							cb(null, profile);
-							return;
-						}
-						});
-			} else {
-				db.close();
-				cb(null, profile);
-				return;
+									});
+							} else {
+								db.close();
+								cb(null, profile);
+								return;
+							}
+							});
+				} else {
+					db.close();
+					cb(null, profile);
+					return;
+				}
 			}
 		});
 };
